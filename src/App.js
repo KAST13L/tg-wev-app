83158a6b8f91e9f4a72666bb5b6375861c8e065f
Davyd Smyk <davyd.s@crypto-hunters.io>
2024-12-22 12:50:31 +0200

    feat!

--- src/App.js
+++ src/App.js
@@ -2,5 +2,10 @@ import { useTelegram } from "./hooks/useTelegram";
 
 export const App = () => {
   const { user } = useTelegram();
-  return <div className="App">Yo!{user?.username}</div>;
+  return (
+    <div className="App">
+      Yo!
+      {user?.username},{user?.first_name},{user?.last_name}
+    </div>
+  );
 };
